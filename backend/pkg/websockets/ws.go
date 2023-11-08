2181e21c951bd18bac38bfffd2f1914b29aa2784
rrahman <rashidur92@hotmail.com>
2023-11-08 17:59:54 +0000

    got basic websockets working

--- backend/pkg/websockets/ws.go
+++ backend/pkg/websockets/ws.go
@@ -1,10 +1,50 @@
 package ws
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gorilla/websocket"
 )
 
-var upgrader = websocket.Upgrader(
-	ReadBufferSize: 1024,
+var Upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-)
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		messageType, message, err := conn.ReadMessage()
+		fmt.Println("message type: ", messageType, "p: ", message, "err: ", err)
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				fmt.Println("error reading message, unexpected closeError: ", err)
+			}
+			fmt.Println("error reading message, NOT unexpected closeError: ", err)
+			return
+		}
+		fmt.Println("message recieved: ", string(message))
+		if err := conn.WriteMessage(messageType, message); err != nil {
+			fmt.Println("error writing message: ", err)
+			return
+		}
+	}
+}
+
+func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	conn, err := Upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("unable to upgrade to websocket: ", err)
+		return
+	}
+	defer conn.Close()
+	fmt.Println("Client Connected")
+	err = conn.WriteMessage(1, []byte("Hi Client!"))
+	if err != nil {
+		fmt.Println("writing message error: ", err)
+	}
+	reader(conn)
+}
--- backend/server.go
+++ backend/server.go
@@ -1,5 +1,25 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"log"
+	"net/http"
+	ws "social-network/backend/pkg/websockets"
+)
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Home Page")
+}
+
+// func wsEndpoint(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "websocket endpoint")
+// 	ws.Upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+// 	ws, err = ws.
+// }
+
+func main() {
+	fmt.Println("main func started")
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/ws", ws.WsEndpoint)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
